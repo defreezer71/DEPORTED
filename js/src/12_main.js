ad85f10dcafcb62a22b7b8835002d6a35e7bc1d6
David DeVries <daviddevries3@gmail.com>
2026-03-15 19:30:02 -0700

    fix: victory song starts at 45s at volume 0.85

--- js/src/12_main.js
+++ js/src/12_main.js
@@ -178,7 +178,7 @@ function update() {
       setTimeout(() => {
         winScreen.classList.add('show');
         const music = document.getElementById('menu-music');
-        if (music) { music.currentTime = 41; music.volume = 0.80; music.play(); }
+        if (music) { music.currentTime = 43; music.volume = 0.85; music.play(); }
       }, 500);
     }
   }
