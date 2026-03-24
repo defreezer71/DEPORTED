07943d00fa80655efde9a583c512ab822c1e2acc
David DeVries <daviddevries3@gmail.com>
2026-03-24 08:24:40 -0700

    debug: log every world tick player list

--- js/game.js
+++ js/game.js
@@ -4092,6 +4092,7 @@ function connectToServer() {
 
       case 'world':
         state.lastServerTick = msg.tick;
+        console.log('WORLD tick', msg.tick, 'players:', msg.players.map(p => p.id + '(' + p.x.toFixed(1) + ',' + p.y.toFixed(1) + ',' + p.z.toFixed(1) + ')').join(' | '));
         updateRemotePlayers(msg.players);
         break;
 
